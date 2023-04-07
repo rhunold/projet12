3676b8de75dca9773568a4bb36add3bf4e385775
Hunold <raphael.hunold@gmail.com>
2023-04-07 16:40:40 +0200

    cleaning url

--- epic_events/epic_events/urls.py
+++ epic_events/epic_events/urls.py
@@ -1,45 +1,39 @@
-
 from django.contrib import admin
 from django.urls import path, include
-# from authentication.admin import manager_site
+
+from rest_framework import routers
 
 
-from crm.views import ClientViewset, ContractViewset, EventViewset #SignUpView, ContributorsViewset, ProjectViewset, IssueViewset, CommentViewset
+from crm.views import (EmployeeViewset, 
+                       ClientViewset, ContractViewset, EventViewset,
+                       test_error) 
 
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
-from rest_framework_nested import routers
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 
-client_router = routers.SimpleRouter()
-client_router.register('clients', ClientViewset, basename='clients')
+router = routers.SimpleRouter()
+router.register('employees', EmployeeViewset, basename='employees')
+router.register('clients', ClientViewset, basename='clients')
+router.register('contracts', ContractViewset, basename='contracts')
+router.register('events', EventViewset, basename='events')
 
-contract_router = routers.SimpleRouter()
-contract_router.register('contracts', ContractViewset, basename='contracts')
 
-event_router = routers.SimpleRouter()
-event_router.register('events', EventViewset, basename='events')
-
-# users_router = routers.NestedSimpleRouter(client_router, 'clients', lookup='clients')
-# users_router.register('users', ContractViewset, basename='contracts')
-
-# issues_router = routers.NestedSimpleRouter(project_router, 'projects', lookup='project')
-# issues_router.register('issues', IssueViewset, basename='issues')
-
-# comments_router = routers.NestedSimpleRouter(issues_router, 'issues', lookup='issue')
-# comments_router.register('comments', CommentViewset, basename='comments')
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('manager_admin/', manager_site.urls),
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+
+    # Web browsable API (for login and logout)
+    path("api/", include("rest_framework.urls", namespace='rest_framework')),
+    
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('', include(client_router.urls)),
-    path('', include(contract_router.urls)),
-    path('', include(event_router.urls)),    
-    # path('', include(issues_router.urls)),
-    # path('', include(comments_router.urls)),    
+    
+    # Endpoint API    
+    path('', include(router.urls)),
+    
+    path('test_error/',test_error, name="test_error")
+
 ]
 
 
